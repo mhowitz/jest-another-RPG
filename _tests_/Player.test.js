9df9da656e97eb0785b122c15c8b3447c41aee54
mikwitz <mikaylahowitz@gmail.com>
2021-12-24 00:33:00 -0700

    added prototype to PLayer constructor

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -1,4 +1,4 @@
-const { expect } = require('@jest/globals');
+
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion');
@@ -14,4 +14,23 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     )
+});
+
+test('get players stats', () => { 
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory= [];
+
+    expect(player.getInventory()).toEqual(false);
 })
